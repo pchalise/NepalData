f77de9f67abc91cc51dea63affe00039c4aa6ef2
Parik Chalise <parikchalise@Pariks-MacBook-Air.local>
2023-06-19 19:51:53 -0400

    final updates

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let chart; // Variable to store the chart instance
+
   // Load the data
   fetch('https://raw.githubusercontent.com/pchalise/NepalData/main/nepal.csv')
     .then(response => response.text())
@@ -7,13 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
       const df = parsedData.data;
 
       const dropdown = document.getElementById('indicator-dropdown');
-      dropdown.style.width = '300px'; // Adjust the width as needed
+      dropdown.style.width = '35%';
 
       const searchInput = document.getElementById('search-input');
 
       // Function to update the dropdown options based on the search term
       function updateDropdownOptions(searchTerm) {
-        const filteredOptions = parsedData.meta.fields.filter(indicator => indicator.toLowerCase().includes(searchTerm.toLowerCase()));
+        const filteredOptions = parsedData.meta.fields.filter(indicator =>
+          indicator.toLowerCase().includes(searchTerm.toLowerCase())
+        );
 
         dropdown.innerHTML = '';
         filteredOptions.forEach(option => {
@@ -40,36 +44,33 @@ document.addEventListener('DOMContentLoaded', function() {
       dropdown.addEventListener('change', dropdownEventHandler);
       searchInput.addEventListener('input', searchInputEventHandler);
 
-      // Function to plot the line graph
+      // Function to plot the Line graph
       function plotLineGraph(indicator) {
         const canvas = document.getElementById('chart-canvas');
         const ctx = canvas.getContext('2d');
-        canvas.width = 700; // Adjust the width as needed
-        canvas.height = 620; // Adjust the height as needed
 
-        // Clear the canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // Destroy the existing chart if it exists
+        if (chart) {
+          chart.destroy();
+        }
 
-        const data = df.map(row => parseFloat(row[indicator]));
-        const years = df.map(row => row['Year']);
+        const data = df.map((row, index) => parseFloat(row[indicator]));
+        const years = df.map((row, index) => String(index + 2000)); // Generate years dynamically
 
-        const chart = new Chart(ctx, {
+        chart = new Chart(ctx, {
           type: 'line',
           data: {
-            labels: years,
-            datasets: [{
-              label: `Trend of ${indicator} from 2000 to 2020`,
-              data: data,
-              fill: false,
-              borderColor: 'blue',
-              backgroundColor: 'transparent',
-              pointBackgroundColor: 'blue',
-              pointRadius: 4,
-              pointHoverRadius: 6,
-              pointHitRadius: 6,
-              pointBorderWidth: 2,
-              pointHoverBorderWidth: 2
-            }]
+            labels: years.slice(0, -1), // Display up to 2020
+            datasets: [
+              {
+                label: `Trend of ${indicator} from 2000 to 2020`,
+                data: data.slice(0, -1), // Display up to 2020
+                backgroundColor: 'transparent',
+                borderColor: 'gray',
+                borderWidth: 2,
+                fill: false
+              }
+            ]
           },
           options: {
             responsive: true,
@@ -78,13 +79,25 @@ document.addEventListener('DOMContentLoaded', function() {
               x: {
                 title: {
                   display: true,
-                  text: 'Year'
+                  text: 'Year',
+                  font: {
+                    size: 12 // Increase the heading size
+                  }
+                },
+                grid: {
+                  color: 'gray',
+                  borderColor: 'gray',
+                  borderWidth: 1
                 }
               },
               y: {
                 title: {
-                  display: true,
-                  text: indicator
+                  display: false
+                },
+                grid: {
+                  color: 'gray',
+                  borderColor: 'gray',
+                  borderWidth: 0.3
                 }
               }
             }
@@ -93,11 +106,8 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
       // Display the initial plot
-      const initialIndicator = parsedData.meta.fields[1];
+      const initialIndicator = parsedData.meta.fields[0];
       updateDropdownOptions('');
       plotLineGraph(initialIndicator);
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
     });
 });
